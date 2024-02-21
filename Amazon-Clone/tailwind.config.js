c51bce73c7d8af19e192f390c34670d32d48b3e9
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-02-21 23:29:36 +0530

    Update tailwind.config.js

--- Amazon-Clone/tailwind.config.js
+++ Amazon-Clone/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
-module.exports = withMT({
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+export default {
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"node_modules/flowbite-react/lib/esm/**/*.js",
+	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
-});
+	plugins: [require("flowbite/plugin")],
+};
