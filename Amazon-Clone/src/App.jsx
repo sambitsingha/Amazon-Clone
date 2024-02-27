29555d709256f41cf4c4cf57676c0ae37addf6da
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-02-27 20:00:58 +0530

    Add Footer component to App.jsx

--- Amazon-Clone/src/App.jsx
+++ Amazon-Clone/src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/navbar";
 import Hero from "./components/carousel";
 import Card from "./components/card";
+import Footer from "./components/footer";
 const App = () => {
 	return (
 		<>
 			<Navbar />
 			<Hero />
 			<Card />
+			<Footer />
 		</>
 	);
 };
