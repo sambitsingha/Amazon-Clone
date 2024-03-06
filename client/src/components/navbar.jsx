25da6158df1ee7341bf0189d8016e8be37de13a5
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-03-06 14:01:13 +0530

    commit

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import logo from "../assets/Amazon-Emblem.png";
 const Navbar = () => {
 	return (
 		<>
-			<nav className='w-full h-16 bg-slate-900'></nav>
+			<nav className='w-full h-16 bg-slate-900'>
+				<img src={logo} alt='' className='' />
+			</nav>
 		</>
 	);
 };
