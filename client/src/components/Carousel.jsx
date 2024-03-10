60b31dd9d0c7d0b79e22cc3d315ed3acd3dbf249
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-03-11 01:13:21 +0530

    Update carousel component and package dependencies

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,21 +1,29 @@
 import React from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const Hero = () => {
+	let settings = {
+		dots: true,
+		infinite: true, // Enables infinite scrolling
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true, // Shows navigation arrows
+	};
 	return (
-		<>
-			<Carousel>
-				<div>
-					<img src='https://images-eu.ssl-images-amazon.com/images/G/31/img23/Softlines_JWL_SH_GW_Assets/2024/SSH/Mar/MFD/uNREC/All/v2/3000all._CB580379196_.jpg' />
-				</div>
-				<div>
-					<img src='https://images-eu.ssl-images-amazon.com/images/G/31/OHL_HSS/PC_hero_3000x1200Unrec._CB580537400_.jpg' />
-				</div>
-				<div>
-					<img src='https://images-eu.ssl-images-amazon.com/images/G/31/img23/Consumables/X-GL/Feb5/PC_Hero_1_3000._CB582457311_.jpg' />
-				</div>
-			</Carousel>
-		</>
+		<Slider {...settings}>
+			<div>
+				<img src='https://images-eu.ssl-images-amazon.com/images/G/31/img22/Baby/cnnjpp1/Baby/Homepage_DesktopHeroTemplate_1500x600._CB580021402_.jpg' />
+			</div>
+			<div>
+				<img src='https://images-eu.ssl-images-amazon.com/images/G/31/img23/Beauty/GW/Makeup-PCww._CB580852678_.jpg' />
+			</div>
+			<div>
+				<img src='https://images-eu.ssl-images-amazon.com/images/G/31/img23/Softlines_JWL_SH_GW_Assets/2024/SSH/Mar/MFD/uNREC/All/v2/3000all._CB580379196_.jpg' />
+			</div>
+		</Slider>
 	);
 };
 
