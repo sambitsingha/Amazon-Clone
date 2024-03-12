aad3359a465241ee991a611b27731318e2e1b15d
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-03-12 22:25:36 +0530

    carousel added

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,29 +1,33 @@
 import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
+import { Carousel } from "flowbite-react";
 const Hero = () => {
-	let settings = {
-		dots: true,
-		infinite: true, // Enables infinite scrolling
-		speed: 500,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: true, // Shows navigation arrows
-	};
 	return (
-		<Slider {...settings}>
-			<div>
-				<img src='https://images-eu.ssl-images-amazon.com/images/G/31/img22/Baby/cnnjpp1/Baby/Homepage_DesktopHeroTemplate_1500x600._CB580021402_.jpg' />
+		<>
+			<div className='h-screen'>
+				<Carousel>
+					<img
+						src='https://m.media-amazon.com/images/I/61aURrton0L._SX3000_.jpg'
+						alt='...'
+					/>
+					<img
+						src='https://images-eu.ssl-images-amazon.com/images/G/31/img23/Softlines_JWL_SH_GW_Assets/2024/March/unrec/All/HSBC/3000pc._CB580474950_.jpg'
+						alt='...'
+					/>
+					<img
+						src='https://images-eu.ssl-images-amazon.com/images/G/31/IMG20/Home/2024/Gateway/ATFGW/BAU_GW_Bedsheets_PC_12th._CB580694896_.jpg'
+						alt='...'
+					/>
+					<img
+						src='https://images-eu.ssl-images-amazon.com/images/G/31/img23/Consumables/X-GL/Feb5/PC_Hero_1_3000._CB582457311_.jpg'
+						alt='...'
+					/>
+					<img
+						src='https://images-eu.ssl-images-amazon.com/images/G/31/img22/Unrec/TallHero_3000X1200_Unrec._CB593464763_.jpg'
+						alt='...'
+					/>
+				</Carousel>
 			</div>
-			<div>
-				<img src='https://images-eu.ssl-images-amazon.com/images/G/31/img23/Beauty/GW/Makeup-PCww._CB580852678_.jpg' />
-			</div>
-			<div>
-				<img src='https://images-eu.ssl-images-amazon.com/images/G/31/img23/Softlines_JWL_SH_GW_Assets/2024/SSH/Mar/MFD/uNREC/All/v2/3000all._CB580379196_.jpg' />
-			</div>
-		</Slider>
+		</>
 	);
 };
 
