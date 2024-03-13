0f95873d765a350985e6176770756a8376a060b4
Sambit <110420002+sambitsingha@users.noreply.github.com>
2024-03-13 22:06:34 +0530

    Add new gradient background to tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,7 +6,14 @@ export default {
 		"node_modules/flowbite-react/lib/esm/**/*.js",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: (theme) => ({
+				"gradient-bg":
+					"linear-gradient(to bottom, #4178ce, #8296da, #b2b7e6, #dbdaf2, #ffffff)",
+				"new-gradient-bg":
+					"linear-gradient(to bottom, #132962, #555787, #8d8bae, #c6c3d6, #ffffff)", // New gradient
+			}),
+		},
 	},
 	plugins: [require("flowbite/plugin")],
 };
